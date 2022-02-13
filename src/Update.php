86d998aa7b2be741f555f508404d028d8f127599
Nazmul Ahsan <n.mukto@gmail.com>
2022-02-13 02:30:09 +0000

    Update.php edited online with Bitbucket

--- src/Update.php
+++ src/Update.php
@@ -113,7 +113,7 @@ class Update {
 		global $pagenow;
 
 		if ( ! is_object( $_transient_data ) ) {
-			$_transient_data = new stdClass;
+			$_transient_data = new \stdClass;
 		}
 
 		if ( 'plugins.php' == $pagenow && is_multisite() ) {
